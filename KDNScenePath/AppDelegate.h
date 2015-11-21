80b031a763cb8c292b89d63b809964404c7f2bfb
duykienvp <duykienvp@yahoo.com.vn>
2015-11-21 14:29:43 -0800

    Initial Commit

---
+++ KDNScenePath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KDNScenePath
+//
+//  Created by Kien Nguyen Duy on 11/21/15.
+//  Copyright © 2015 Kien Nguyen Duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KDNScenePath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KDNScenePath
+//
+//  Created by Kien Nguyen Duy on 11/21/15.
+//  Copyright © 2015 Kien Nguyen Duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
